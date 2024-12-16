644de02a92449a3762814dae1cfd1fc00e15a265
Yoon Seonwoong <neuron4722@naver.com>
2024-12-16 20:33:38 +0900

    add 설탕배달.py

---
+++ Seonwoong/week 1/설탕배달.py
@@ -0,0 +1,19 @@
+# 소요시간 : 1-2분
+# 시간복잡도 : O(N)
+# https://www.acmicpc.net/problem/2839
+
+N=int(input())
+
+dp=[-1]*(5001)
+dp[3],dp[5]=1,1
+
+if N>5:
+    for i in range(5, N+1):
+        if dp[i-3]!= -1 and dp[i-5]!=-1:
+            dp[i]=min(dp[i-3],dp[i-5])+1
+        elif dp[i-3]==-1 and dp[i-5]!=-1:
+            dp[i]=dp[i-5]+1
+        elif dp[i-3]!=-1 and dp[i-5]==-1:
+            dp[i]=dp[i-3]+1
+
+print(dp[N]) 
