768c806c19f14e0adec644664d314ad5fc14def9
JEONG EUN KIM <121777522+wjddms4299@users.noreply.github.com>
2025-01-10 10:51:59 +0900

    Create 동전 0.py

---
+++ jeongeun/동전 0.py
@@ -0,0 +1,11 @@
+N, K = map(int, input().split())
+coins = [int(input()) for _ in range(N)]
+
+count = 0
+for coin in reversed(coins):
+    if K == 0:
+        break
+    count += K // coin
+    K %= coin
+
+print(count)
