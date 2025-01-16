e00a2ef0a961da188085e02a3f8d8740eebb989b
JEONG EUN KIM <121777522+wjddms4299@users.noreply.github.com>
2025-01-16 09:59:45 +0900

    Create 가장 긴 증가하는 부분 수열.py

---
+++ jeongeun/가장 긴 증가하는 부분 수열.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.read
+data = input().split()
+
+N = int(data[0])
+A = list(map(int, data[1:]))
+
+dp = [1] * N
+
+for i in range(1, N):
+    for j in range(i):
+        if A[j] < A[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+            
+print(max(dp))
